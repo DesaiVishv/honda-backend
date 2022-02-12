6c995aa23bde53084b6b5f4040cf88fb8c866984
Hiren Ramoliya <hiren086.rejoice@gmail.com>
2022-02-12 10:09:48 +0530

    make api of get information by id

--- src/api/information/index.js
+++ src/api/information/index.js
@@ -1,12 +1,13 @@
 const addInformation = require("./add-information");
 const getAllInformation = require("./get-all-information");
+const getInformationById = require("./get-information-by-id");
 const updateInformation = require("./update-information");
 const deleteInformation = require("./delete-information");
 
-
 module.exports = exports = {
-    addInformation,
-    getAllInformation,
-    updateInformation,
-    deleteInformation
-}
+  addInformation,
+  getAllInformation,
+  getInformationById,
+  updateInformation,
+  deleteInformation,
+};
