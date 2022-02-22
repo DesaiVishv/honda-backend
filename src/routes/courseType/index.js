ba072cda230794637af8efc500dd36b77a7e45f3
Meet Gajera <meet103.rejoice@gmail.com>
2022-02-22 10:39:49 +0530

    changes in courseType and vehicleCategory schema

--- src/routes/courseType/index.js
+++ src/routes/courseType/index.js
@@ -9,19 +9,36 @@ router.get("/getAllCourseType", courseTypeApi.getAllCourseType.handler);
 router.get("/getAll", courseTypeApi.getAllForDownload.handler);
 
 // router.post("/getSubmenuByMenu",   vehicleCategoryApi.getSubmenuByMenu.handler);
-router.post("/getCoursetypeByVehiclecategory",   courseTypeApi.getCoursetypeByVehiclecategory.handler);
-
+router.post(
+  "/getCoursetypeByVehiclecategory",
+  courseTypeApi.getCoursetypeByVehiclecategory.handler
+);
 
 // Post Methods
-router.post("/addCourseType", passport.authenticate(["jwt"], { session: false }), validate("body", courseTypeApi.addCourseType.validation), courseTypeApi.addCourseType.handler);
+router.post(
+  "/addCourseType",
+  validate("body", courseTypeApi.addCourseType.validation),
+  courseTypeApi.addCourseType.handler
+);
 
 // // Put Methods
-router.put("/updateCourseType/:id", passport.authenticate(["jwt"], { session: false }), courseTypeApi.updateCourseType.handler);
+router.put(
+  "/updateCourseType/:id",
+  passport.authenticate(["jwt"], { session: false }),
+  courseTypeApi.updateCourseType.handler
+);
 
-router.put("/updateStatus/:id", passport.authenticate(["jwt"], { session: false }), courseTypeApi.updateStatus.handler);
-
+router.put(
+  "/updateStatus/:id",
+  passport.authenticate(["jwt"], { session: false }),
+  courseTypeApi.updateStatus.handler
+);
 
 // // Delete Methods
-router.delete("/deleteCourseType/:id", passport.authenticate(["jwt"], { session: false }), courseTypeApi.deleteCourseType.handler);
+router.delete(
+  "/deleteCourseType/:id",
+  passport.authenticate(["jwt"], { session: false }),
+  courseTypeApi.deleteCourseType.handler
+);
 
 module.exports = exports = router;
--- src/schema/courseType/courseType.js
+++ src/schema/courseType/courseType.js
@@ -3,28 +3,30 @@ const mongoose = require("mongoose");
 // const enums = require("../../../json/enums.json");
 
 module.exports = (connection) => {
-    const courseTypeSchema = new mongoose.Schema({
-        
-        courseType:{ type: String, require:true },
-        description:{type:String, require:true},
-        isActive :{type:Boolean, default:true},
-        vcid:{ type: mongoose.Schema.Types.ObjectId},
+  const courseTypeSchema = new mongoose.Schema(
+    {
+      courseType: { type: String, require: true },
+      description: { type: String, require: true },
+      isActive: { type: Boolean, default: true },
+      vcid: { type: mongoose.Schema.Types.ObjectId },
+      isDelete: { type: Boolean, default: false },
 
-        createdAt: { type: Date, default: Date.now },
-        updatedAt: { type: Date, default: Date.now },
-        createdBy: {
-            type: String,
-            default: "Admin",
-        },
-        updatedBy: {
-            type: String,
-            default: "Admin",
-        },
-    }, {
-        autoCreate: true
-    });
+      createdAt: { type: Date, default: Date.now },
+      updatedAt: { type: Date, default: Date.now },
+      createdBy: {
+        type: String,
+        default: "Admin",
+      },
+      updatedBy: {
+        type: String,
+        default: "Admin",
+      },
+    },
+    {
+      autoCreate: true,
+    }
+  );
 
-    // return logsSchema;
-    return connection.model("courseType", courseTypeSchema, "courseType");
+  // return logsSchema;
+  return connection.model("courseType", courseTypeSchema, "courseType");
 };
-
