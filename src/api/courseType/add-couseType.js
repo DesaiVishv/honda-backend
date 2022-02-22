5e3d54242db853b6f20c83d375c7030431865cdc
Meet Gajera <meet103.rejoice@gmail.com>
2022-02-22 10:40:56 +0530

    changes in add course Type

--- src/api/courseType/add-couseType.js
+++ src/api/courseType/add-couseType.js
@@ -8,79 +8,88 @@ const utils = require("../../utils");
 
 // Add category by admin
 module.exports = exports = {
-    // route validation
-    validation: Joi.object({
-        courseType: Joi.string().required(),
-        vcid: Joi.string(),
-        description: Joi.string().required(),
-        isActive: Joi.boolean().required()
-        // imagePath: Joi.string().allow("")
-    }),
+  // route validation
+  validation: Joi.object({
+    courseType: Joi.string().required(),
+    vcid: Joi.string(),
+    description: Joi.string().required(),
+    isActive: Joi.boolean().required(),
+    // imagePath: Joi.string().allow("")
+  }),
 
-    handler: async (req, res) => {
-        const { courseType, vcid, description, isActive } = req.body;
-        const { user } = req;
-        if (user.type !== enums.USER_TYPE.SUPERADMIN) {
-            const data4createResponseObject = {
-                req: req,
-                result: -1,
-                message: messages.NOT_AUTHORIZED,
-                payload: {},
-                logPayload: false
-            };
-            return res.status(enums.HTTP_CODES.UNAUTHORIZED).json(utils.createResponseObject(data4createResponseObject));
-        }
-        if (!courseType || !vcid || !description) {
-            const data4createResponseObject = {
-                req: req,
-                result: -1,
-                message: messages.FILL_DETAILS,
-                payload: {},
-                logPayload: false
-            };
-            return res.status(enums.HTTP_CODES.BAD_REQUEST).json(utils.createResponseObject(data4createResponseObject));
-        }
-
-        try {
-
-            // const checkMenu = await global.models.GLOBAL.COURSETYPE.find({ courseType:courseType});
-            // if (checkMenu.length > 0) {
-            //     const data4createResponseObject = {
-            //         req: req,
-            //         result: -400,
-            //         message: messages.EXISTS_MENU,
-            //         payload: {},
-            //         logPayload: false
-            //     };
-            //     res.status(enums.HTTP_CODES.OK).json(utils.createResponseObject(data4createResponseObject));
-            //     return;
-            // }
-            let AmenintiesCreate = {
-                courseType: courseType,
-                vcid: vcid,
-                description: description,
-                isActive: isActive
-            };
-            const newAmeninties = await global.models.GLOBAL.COURSETYPE(AmenintiesCreate);
-            newAmeninties.save();
-            const data4createResponseObject = {
-                req: req,
-                result: 0,
-                message: messages.ITEM_INSERTED,
-                payload: { newAmeninties },
-                logPayload: false
-            };
-            res.status(enums.HTTP_CODES.OK).json(utils.createResponseObject(data4createResponseObject));
-        } catch (error) {
-            logger.error(`${req.originalUrl} - Error encountered: ${error.message}\n${error.stack}`);
-            const data4createResponseObject = {
-                req: req,
-                result: -1,
-                message: messages.GENERAL,
-                payload: {},
-                logPayload: false
-            };
-            res.status(enums.HTTP_CODES.INTERNAL_SERVER_ERROR).json(utils.createResponseObject(data4createResponseObject));
-        }
+  handler: async (req, res) => {
+    const { courseType, vcid, description, isActive } = req.body;
+    const { user } = req;
+    // if (user.type !== enums.USER_TYPE.SUPERADMIN) {
+    //     const data4createResponseObject = {
+    //         req: req,
+    //         result: -1,
+    //         message: messages.NOT_AUTHORIZED,
+    //         payload: {},
+    //         logPayload: false
+    //     };
+    //     return res.status(enums.HTTP_CODES.UNAUTHORIZED).json(utils.createResponseObject(data4createResponseObject));
+    // }
+    if (!courseType || !vcid || !description) {
+      const data4createResponseObject = {
+        req: req,
+        result: -1,
+        message: messages.FILL_DETAILS,
+        payload: {},
+        logPayload: false,
+      };
+      return res
+        .status(enums.HTTP_CODES.BAD_REQUEST)
+        .json(utils.createResponseObject(data4createResponseObject));
     }
+
+    try {
+      // const checkMenu = await global.models.GLOBAL.COURSETYPE.find({ courseType:courseType});
+      // if (checkMenu.length > 0) {
+      //     const data4createResponseObject = {
+      //         req: req,
+      //         result: -400,
+      //         message: messages.EXISTS_MENU,
+      //         payload: {},
+      //         logPayload: false
+      //     };
+      //     res.status(enums.HTTP_CODES.OK).json(utils.createResponseObject(data4createResponseObject));
+      //     return;
+      // }
+      let AmenintiesCreate = {
+        courseType: courseType,
+        vcid: vcid,
+        description: description,
+        isActive: isActive,
+      };
+      const newAmeninties = await global.models.GLOBAL.COURSETYPE(
+        AmenintiesCreate
+      );
+      newAmeninties.save();
+      const data4createResponseObject = {
+        req: req,
+        result: 0,
+        message: messages.ITEM_INSERTED,
+        payload: { newAmeninties },
+        logPayload: false,
+      };
+      res
+        .status(enums.HTTP_CODES.OK)
+        .json(utils.createResponseObject(data4createResponseObject));
+    } catch (error) {
+      logger.error(
+        `${req.originalUrl} - Error encountered: ${error.message}\n${error.stack}`
+      );
+      const data4createResponseObject = {
+        req: req,
+        result: -1,
+        message: messages.GENERAL,
+        payload: {},
+        logPayload: false,
+      };
+      res
+        .status(enums.HTTP_CODES.INTERNAL_SERVER_ERROR)
+        .json(utils.createResponseObject(data4createResponseObject));
+    }
+  },
 };
