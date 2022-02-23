5c05de2b0fab39b94c70da3b022b1d164c42a3a8
Meet Gajera <meet103.rejoice@gmail.com>
2022-02-23 17:05:27 +0530

    changes in get records by range

--- src/api/register/get-records-by-range.js
+++ src/api/register/get-records-by-range.js
@@ -12,14 +12,14 @@ module.exports = exports = {
 
   handler: async (req, res) => {
     try {
-      let { startDate } = req.query;
-      let { endDate } = req.query;
+      let { sd } = req.query;
+      let { ed } = req.query;
       let dateFilter = {};
-      if (startDate) {
+      if (sd) {
         dateFilter = {
           $and: [
-            { createdAt: { $gte: new Date(startDate) } },
-            { createdAt: { $lte: new Date(endDate) } },
+            { createdAt: { $gte: new Date(sd) } },
+            { createdAt: { $lte: new Date(ed) } },
           ],
         };
       }
@@ -30,93 +30,79 @@ module.exports = exports = {
         ? { fname: { $regex: req.query.search, $options: "i" }, ...dateFilter }
         : { ...dateFilter };
 
-      if (!startDate || !endDate) {
+      const count = await global.models.GLOBAL.REGISTER.find(search).count();
+      console.log("count", count);
+      let findUser = await global.models.GLOBAL.REGISTER.aggregate([
+        {
+          $match: search,
+        },
+        {
+          $lookup: {
+            from: "vehicleCategory",
+            localField: "vcid",
+            foreignField: "_id",
+            as: "vehicleCategory",
+          },
+        },
+
+        {
+          $lookup: {
+            from: "courseType",
+            localField: "ctid",
+            foreignField: "_id",
+            as: "courseType",
+          },
+        },
+        {
+          $lookup: {
+            from: "courseName",
+            localField: "cnid",
+            foreignField: "_id",
+            as: "courseName",
+          },
+        },
+        {
+          $lookup: {
+            from: "trainingDate",
+            localField: "tdid",
+            foreignField: "_id",
+            as: "trainingDate",
+          },
+        },
+        {
+          $lookup: {
+            from: "admin",
+            localField: "uid",
+            foreignField: "_id",
+            as: "uid",
+          },
+        },
+        {
+          $unwind: { path: "$uid", preserveNullAndEmptyArrays: true },
+        },
+      ]).sort({ createdAt: -1 });
+      if (!findUser) {
         const data4createResponseObject = {
           req: req,
-          result: -1,
-          message: messages.INVALID_PARAMETERS,
+          result: 0,
+          message: messages.NOT_FOUND,
           payload: {},
           logPayload: false,
         };
         res
           .status(enums.HTTP_CODES.OK)
           .json(utils.createResponseObject(data4createResponseObject));
-        return;
       } else {
-        const count = await global.models.GLOBAL.REGISTER.find(search).count();
-        console.log("count", count);
-        let findUser = await global.models.GLOBAL.REGISTER.aggregate([
-          {
-            $match: search,
-          },
-          {
-            $lookup: {
-              from: "vehicleCategory",
-              localField: "vcid",
-              foreignField: "_id",
-              as: "vehicleCategory",
-            },
-          },
-
-          {
-            $lookup: {
-              from: "courseType",
-              localField: "ctid",
-              foreignField: "_id",
-              as: "courseType",
-            },
-          },
-          {
-            $lookup: {
-              from: "courseName",
-              localField: "cnid",
-              foreignField: "_id",
-              as: "courseName",
-            },
-          },
-          {
-            $lookup: {
-              from: "trainingDate",
-              localField: "tdid",
-              foreignField: "_id",
-              as: "trainingDate",
-            },
-          },
-          {
-            $lookup: {
-              from: "admin",
-              localField: "uid",
-              foreignField: "_id",
-              as: "uid",
-            },
-          },
-          {
-            $unwind: { path: "$uid", preserveNullAndEmptyArrays: true },
-          },
-        ]).sort({ createdAt: -1 });
-        if (!findUser) {
-          const data4createResponseObject = {
-            req: req,
-            result: 0,
-            message: messages.NOT_FOUND,
-            payload: {},
-            logPayload: false,
-          };
-          res
-            .status(enums.HTTP_CODES.OK)
-            .json(utils.createResponseObject(data4createResponseObject));
-        } else {
-          const data4createResponseObject = {
-            req: req,
-            result: 0,
-            message: messages.SUCCESS,
-            payload: { Question: findUser, count: count },
-            logPayload: false,
-          };
-          res
-            .status(enums.HTTP_CODES.OK)
-            .json(utils.createResponseObject(data4createResponseObject));
-        }
+        const data4createResponseObject = {
+          req: req,
+          result: 0,
+          message: messages.SUCCESS,
+          payload: { Question: findUser, count: count },
+          logPayload: false,
+        };
+        res
+          .status(enums.HTTP_CODES.OK)
+          .json(utils.createResponseObject(data4createResponseObject));
       }
     } catch (error) {
       logger.error(
