e517011dc57dc2ce300f8314f09ecaef6dbba29c
Meet Gajera <meet103.rejoice@gmail.com>
2022-02-23 15:27:40 +0530

    add field in announcement flow

--- src/schema/Announcement/Announcement.js
+++ src/schema/Announcement/Announcement.js
@@ -3,29 +3,31 @@ const mongoose = require("mongoose");
 // const enums = require("../../../json/enums.json");
 
 module.exports = (connection) => {
-    const announcementSchema = new mongoose.Schema({
+  const announcementSchema = new mongoose.Schema(
+    {
+      name: { type: String, require: true },
+      type: { type: String, require: true },
+      image: { type: String, default: null },
+      description: { type: String, require: true },
+      status: { type: Boolean, default: true },
+      date: { type: Date, require: true },
+      isActive: { type: Boolean, default: true },
+      createdAt: { type: Date, default: Date.now },
+      updatedAt: { type: Date, default: Date.now },
+      createdBy: {
+        type: String,
+        default: "Admin",
+      },
+      updatedBy: {
+        type: String,
+        default: "Admin",
+      },
+    },
+    {
+      autoCreate: true,
+    }
+  );
 
-        name:{type:String, require:true},
-        type:{type:String, require:true},
-        image:{type:String, default:null},
-        description:{type:String, require:true},
-        status:{type:Boolean, default:true},
-        date:{type:Date, require:true},
-        createdAt: { type: Date, default: Date.now },
-        updatedAt: { type: Date, default: Date.now },
-        createdBy: {
-            type: String,
-            default: "Admin",
-        },
-        updatedBy: {
-            type: String,
-            default: "Admin",
-        },
-    }, {
-        autoCreate: true
-    });
-
-    // return logsSchema;
-    return connection.model("announcement", announcementSchema, "announcement");
+  // return logsSchema;
+  return connection.model("announcement", announcementSchema, "announcement");
 };
-
