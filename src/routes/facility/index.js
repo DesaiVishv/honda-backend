543e542f83747099b811a82c042e2c843155b4d9
Meet <meet103.rejoice@gmail.com>
2022-04-13 11:59:06 +0530

    make api of get all facility

--- src/routes/facility/index.js
+++ src/routes/facility/index.js
@@ -6,15 +6,27 @@ const passport = require("passport");
 
 // Get Methods
 router.get("/getFacility/:id", facilityApi.getFacility.handler);
-// router.get("/getAllMenu", menuApi.getAllMenu.handler);
+router.get("/getAllFacility", facilityApi.getAllFacility.handler);
 
 // Post Methods
-router.post("/addFacility", validate("body", facilityApi.addFacility.validation), facilityApi.addFacility.handler);
+router.post(
+  "/addFacility",
+  validate("body", facilityApi.addFacility.validation),
+  facilityApi.addFacility.handler
+);
 
 // // Put Methods
-router.put("/updateFacility/:id", passport.authenticate(["jwt"], { session: false }), facilityApi.updateFacility.handler);
+router.put(
+  "/updateFacility/:id",
+  passport.authenticate(["jwt"], { session: false }),
+  facilityApi.updateFacility.handler
+);
 
 // // Delete Methods
-router.delete("/deleteFacility/:id", passport.authenticate(["jwt"], { session: false }), facilityApi.deleteFacility.handler);
+router.delete(
+  "/deleteFacility/:id",
+  passport.authenticate(["jwt"], { session: false }),
+  facilityApi.deleteFacility.handler
+);
 
 module.exports = exports = router;
