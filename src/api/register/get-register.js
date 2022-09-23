162a2768031a15491bf1ab5cf82c1e0118e9285d
prince172.rejoice <prince172.rejoice@gmail.com>
2022-09-23 13:59:48 +0530

    changes in getRegister

--- src/api/register/get-register.js
+++ src/api/register/get-register.js
@@ -105,6 +105,65 @@ module.exports = exports = {
         .limit(limit);
       Questions = JSON.parse(JSON.stringify(Questions));
       for await (let values of Questions) {
+        // if (values.uid) {
+        // let id = values.uid._id;
+        let tdid = await global.models.GLOBAL.REGISTER.find({}).distinct("tdid");
+        let uptd = await global.models.GLOBAL.TRAININGDATE.find({
+          endTime: { $gte: new Date(Date.now()) },
+          _id: { $in: tdid },
+        }).distinct("_id");
+        let pstd = await global.models.GLOBAL.TRAININGDATE.find({
+          endTime: { $lt: new Date(Date.now()) },
+          _id: { $in: tdid },
+        }).distinct("_id");
+        let isUpcomming = await global.models.GLOBAL.REGISTER.find({
+          // uid: id,
+          tdid: { $in: uptd },
+          isCancle: false,
+        });
+        isUpcomming.map((item) => {
+          if (item._id.toString() == values._id.toString()) {
+            values.recordType = "upcomming";
+          }
+        });
+        let isPast = await global.models.GLOBAL.REGISTER.find({
+          // uid: id,
+          tdid: { $in: pstd },
+          isCancle: false,
+        });
+        isPast.map((item) => {
+          if (item._id.toString() == values._id.toString()) {
+            values.recordType = "past";
+          }
+        });
+        let isCancle = await global.models.GLOBAL.REGISTER.find({
+          // uid: id,
+          isCancle: true,
+        });
+        isCancle.map((item) => {
+          if (item._id.toString() == values._id.toString()) {
+            values.recordType = "cancle";
+          }
+        });
+        // isUpcomming = JSON.parse(JSON.stringify(isUpcomming));
+        // if (isUpcomming.length > 0) {
+        //   isUpcomming[0].recordType = "isUpcomming";
+        //   values.recordType = "isUpcomming";
+        // }
+        // isPast = JSON.parse(JSON.stringify(isPast));
+        // if (isPast.length > 0) {
+        //   isPast[0].recordType = "isPast";
+        //   values.recordType = "isPast";
+        // }
+        // isCancle = JSON.parse(JSON.stringify(isCancle));
+        // if (isCancle.length > 0) {
+        //   isCancle[0].recordType = "cancle";
+        //   values.recordType = "cancle";
+        // }
+        // if (isUpcomming) recordType = "upcomming";
+        // else if (isPast) recordType = "past";
+        // else if (isCancle) recordType = "cancle";
+        // }
         const paymentHistory = await global.models.GLOBAL.PAYMENT.find({
           phone: values.phone,
           status: "done",
@@ -121,9 +180,7 @@ module.exports = exports = {
           payload: {},
           logPayload: false,
         };
-        res
-          .status(enums.HTTP_CODES.OK)
-          .json(utils.createResponseObject(data4createResponseObject));
+        res.status(enums.HTTP_CODES.OK).json(utils.createResponseObject(data4createResponseObject));
         return;
       }
       const data4createResponseObject = {
@@ -133,13 +190,9 @@ module.exports = exports = {
         payload: { Question: Questions, count: count },
         logPayload: false,
       };
-      res
-        .status(enums.HTTP_CODES.OK)
-        .json(utils.createResponseObject(data4createResponseObject));
+      res.status(enums.HTTP_CODES.OK).json(utils.createResponseObject(data4createResponseObject));
     } catch (error) {
-      logger.error(
-        `${req.originalUrl} - Error encountered: ${error.message}\n${error.stack}`
-      );
+      logger.error(`${req.originalUrl} - Error encountered: ${error.message}\n${error.stack}`);
       const data4createResponseObject = {
         req: req,
         result: -1,
@@ -147,9 +200,7 @@ module.exports = exports = {
         payload: {},
         logPayload: false,
       };
-      res
-        .status(enums.HTTP_CODES.INTERNAL_SERVER_ERROR)
-        .json(utils.createResponseObject(data4createResponseObject));
+      res.status(enums.HTTP_CODES.INTERNAL_SERVER_ERROR).json(utils.createResponseObject(data4createResponseObject));
     }
   },
 };
